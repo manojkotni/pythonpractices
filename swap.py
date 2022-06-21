831dbe2ad5d8ac9060ffce00b3ad9048ec3e99aa
manojkotni <99539907+manojkotni@users.noreply.github.com>
2022-06-21 10:55:00 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,12 @@
+Python 3.10.3 (tags/v3.10.3:a342a49, Mar 16 2022, 13:07:40) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+a = 5
+b = 6
+a = a+b
+b =  a-b
+a = a-b
+ 
+print(b)
+5
+print(a)
+6
